51794251f0ba32e1e39d424c3a26cae806242731
Hurricane <15865286016@139.com>
2019-05-29 02:49:07 +0800

    111

--- src/main/java/com/hurricane/learn/jenkins/App.java
+++ src/main/java/com/hurricane/learn/jenkins/App.java
@@ -3,16 +3,13 @@ package com.hurricane.learn.jenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.hurricane.learn.jenkins.controller.UserController;
-
 @SpringBootApplication
 public class App {
 	
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(App.class);
-		application.run(args);
+		application.run(args); 
 //		System.out.println("from java");
 	}
-	
 	
 }
